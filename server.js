4bd68694a0342fa14b0fc4b951ffee4701854415
alikaghamaleki <126207592+alikaghamaleki@users.noreply.github.com>
2025-11-29 02:09:04 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('کاربر وصل شد');
+});
+
+server.listen(3000, () => {
+  console.log('سرور روشن شد → http://localhost:3000');
+});
